860e64fcde132b05a83bd61f661ab891eede643f
Rafal Pawlik <rafal.kamil.pawlik@gmail.com>
2022-07-31 17:59:36 +0200

    Add project files.

---
+++ HandyTaxApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HandyTaxApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
